3ade52e465080587ae0cbf962e21d1a7db286eb4
tuanndph16237 <tuanndph16237@example.com>
2022-10-27 22:18:55 +0700

    crud_ajax in laravel

---
+++ crud-ajax/routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Http\Controllers\EmployeeController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', [EmployeeController::class, 'index']);
+Route::post('/store', [EmployeeController::class, 'store'])->name('store');
+Route::get('/fetchall', [EmployeeController::class, 'fetchAll'])->name('fetchAll');
+Route::delete('/delete', [EmployeeController::class, 'delete'])->name('delete');
+Route::get('/edit', [EmployeeController::class, 'edit'])->name('edit');
+Route::post('/update', [EmployeeController::class, 'update'])->name('update');
