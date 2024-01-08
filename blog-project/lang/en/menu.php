b74a15fd56115ff2c4650c6810c8bf571ff90476
tuanndph16237 <tuanndph16237@example.com>
2024-01-08 18:40:41 +0700

    yelo blog

---
+++ blog-project/lang/en/menu.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'home' => 'Home',
+    'blog' => 'Blog',
+    'admin' => 'Admin',
+    'profile' => 'Profile',
+    'login' => 'Login',
+    'register' => 'Register',
+    'logout' => 'Log out',
+    'manage_account' => 'Manage Account',
+];
